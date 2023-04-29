4319646af53c6d3c7af7e0fffcd3bbda89773840
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-04-29 12:24:14 -0500

    add an endpoint for login

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 
 // npm i express to get the dependency
 const express = require("express");
+const { checkAuth } = require('./middleware');
 // Running on port 8080.
 const PORT = 8080;
 
 // Server with express.
 const server = express();
+server.use(express.json());
+server.use(checkAuth);
 
 // Endpoint created.
 server.get("/heartbeat", (req, res) => {
 	res.json({"is":"working", "status":"good"});
 });
 
+server.post('/auth/login', (req, res) => {
+	res.send(`${req.body.username}, ${req.body.password}`)
+});
+
 server.listen(PORT, () => {
 	console.log(`The server is running at PORT ${PORT}`)
-})
+});
