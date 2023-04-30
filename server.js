53ece25b3c86e0f45a831ab04e3bf238edf8d503
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-04-29 21:48:55 -0500

    add test endpoint to get user from database

--- server.js
+++ server.js
@@ -2,18 +2,41 @@
 // npm i express to get the dependency
 const express = require("express");
 const { checkAuth } = require('./middleware');
+const pgp = require('pg-promise')();
+
 // Running on port 8080.
 const PORT = 8080;
+
+const cn = {
+    host: 'localhost',
+    port: 5432,
+    database: 'users',
+    user: 'jhongarcian',
+    password: '',
+    allowExitOnIdle: true
+}
+
+const db =pgp(cn);
 
 // Server with express.
 const server = express();
 server.use(express.json());
-server.use(checkAuth);
+// server.use(checkAuth);
 
 // Endpoint created.
 server.get("/heartbeat", (req, res) => {
 	res.json({"is":"working", "status":"good"});
 });
+
+server.get("/test", async (req, res) => {
+	const user = await test();
+	res.json(user)
+});
+
+async function test() {
+	const user = await db.any('SELECT * FROM userAuth', [true]);
+	return user
+}
 
 server.post('/auth/login', (req, res) => {
 	res.send(`${req.body.username}, ${req.body.password}`)
